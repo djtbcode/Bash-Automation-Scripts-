7632eedd3f6bedd6e01c18b9eeab5d34772dd7b6
David <129532735+djtbcode@users.noreply.github.com>
2025-04-16 12:02:14 +0100

    Create tree.sh
    Let’s work on a basic script that prints a directory structure in a tree format, which is useful for viewing nested subdirectories. The script takes advantage of the powerful commands that the 
    [Linux](https://www.codecademy.com/resources/docs/open-source/linux) shell provides for file and folder operations.
    
    We will be using conditional statements as well as the following commands:
    
    - `echo`
    - `cd`
    - `pwd`
    - `ls`
    - `grep`
    - `sed`
    - `wc`
    
    This script will take in an optional string argument: the parent directory that should be the starting point for the tree. If this is not provided, we will use the current working directory. We will print a message if the parent directory has no subfolders to print. If we find there are subfolders to print, we’ll use a combination of the commands `ls`, `grep`, and `sed` to print the directory structure tree.
    
    We will be working on the script named `tree.sh`, which is already opened in the editor.

---
+++ tree.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Begin code below:
+# If input argument, cd to the directory in the input argument
+if [ "$1" != "" ]
+then cd "$1"
+fi
+
+pwd
+
+if [ `ls -F -1 | grep "/" | wc -l` = 0 ]
+then echo "     -> no sub-directories"
+fi
+# The provided conditional statement uses `ls` lists files and directories with indicators one line at a time. `grep` is then used to find the `/` indicator which implies a directory. If the line count calculated by `wc -l` is `0`, then no subdirectories exist.
+
+ls -R 
+
